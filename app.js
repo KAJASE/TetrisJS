76fe669b6fae4a831285c5edc630d28365d42419
Karen <kareninejapiassu@gmail.com>
2020-08-27 23:41:47 +0200

    move the tetrominoes left

--- app.js
+++ app.js
@@ -67,6 +67,22 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   timerId = setInterval(moveDown, 1000);
+
+  function control(e) {
+    if(e.keyCode === 37) {
+      moveLeft()
+    }else if(e.keyCode === 38) {
+       rotate()
+    }else if(e.keyCode === 39) {
+      moveRight()
+    }else if(e.keyCode === 40) {
+      moveDown()
+    }
+    
+  }
+  document.addEventListener('keyup', control);
+
+
   function moveDown() {
     undraw();
     currentPosition += width;
@@ -91,5 +107,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  
+  function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index)%width === 0)
+
+    if(!isAtLeftEdge) currentPosition -= 1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+    currentPosition += 1
+  }
+
+  draw()
+}
+
 });
